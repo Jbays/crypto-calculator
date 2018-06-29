d44f57b87035b5f82851027b1756fd23872110a3
jbays <jjbaize@gmail.com>
2018-06-29 13:02:46 -0500

    Everything mocked out for TDD

---
+++ tests/app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('these tests are for routes',()=>{
+
+  it('the root route should respond to requests',()=>{
+    // console.log(request);
+  })
+
+})
