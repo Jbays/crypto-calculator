6ecd6403e331b1cbcc345d8b09f33670aa59825b
jbays <jjbaize@gmail.com>
2018-06-29 13:22:13 -0500

    Including the additional dependencies

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Calculates profitability for crypto assets
 
 ## To Run tests
 `npm run test`
+
+## To Run Server
+`nodemon index.js`
--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+const createError = require('http-errors');
 const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const app = express();
 
 app.get('/',(req,res)=>{
