9f7e831fa188a6675910e50addc5c32527183023
jbays <jjbaize@gmail.com>
2018-06-30 12:07:05 -0500

    Can fetch latest prices from coinmarketcap

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Calculates profitability for crypto assets
 
 ## To Run Server
 `nodemon index.js`
+
+
+### Up to date pricing
+updatePrice.js requests the latest crypto prices for 10 different cryptocurrencies.
+
+Prices are from [CoinMarketCap](https://coinmarketcap.com/)
--- updatePrice.js
+++ updatePrice.js
@@ -1,23 +1,30 @@
 const axios = require('axios');
 
-
 //NOTE: request address
 // 'https://api.coinmarketcap.com/v2/ticker/'
 
+//NOTE: to add more coins, I have to look up
 const coinsInvested = [
-  1, //bitcoin
-  2, //litecoin
+  1,    //bitcoin
   1027, //ethereum
-  1765, //eos
   1839, //binance coin
-  328, //monero
-  131, //dash
-  52, //ripple
-  2010, //cardano
+  328,  //monero
+  131,  //dash
   1908, //nebulas
+  52,   //ripple
+  2010, //cardano
+  1765, //eos
+  2,    //litecoin
 ]
 
+//NOTE: when should this script run?  On opening of the app?
 axios.get('https://api.coinmarketcap.com/v2/ticker/')
   .then((response)=>{
-    console.log("response.data",JSON.stringify(response.data))
+    //probably should use a filter
+    coinsInvested.forEach((cryptoIndex)=>{
+      console.log("this is what I want",response.data['data'][cryptoIndex]['name'])
+      console.log("this is what I want",response.data['data'][cryptoIndex]['symbol'])
+      console.log("this is what I want",response.data['data'][cryptoIndex]['quotes']['USD']['price'])
+    })
+
   })
