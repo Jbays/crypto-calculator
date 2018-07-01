f65d9864ba1af3d5add473dca88618973c5a3536
jbays <jjbaize@gmail.com>
2018-07-01 10:57:10 -0500

    Removed connectToDb for being pointless.  API call for latest prices happens in updatePrice.js now. updatePrice's promise chain hangs -- completes its task then sits and waits...til forever. The cause is unknown.  but probably something SIMPLE.

--- connectToDb.js
+++
@@ -1,7 +0,0 @@
-const env = 'development';
-const config = require('./knexfile')[env];
-const knex = require('knex')(config);
-
-const sql = knex('coins_index').toString();
-
-console.log("sql>>>>>",sql);
