5472cc7aced8ccb2df15c6819dbfd89a0ab9babf
jbays <jjbaize@gmail.com>
2018-07-05 11:52:01 -0500

    Simplified updateBalances.js by removing the abstracted functions.  Abstracting functions is supposed to make code cleaner, more simple.  Not the opposite

--- scripts/updateBalances.js
+++ scripts/updateBalances.js
@@ -15,8 +15,12 @@ knex('purchases(pch)')
   let sumsObj = {};
   let weightsObj = {};
 
-  console.log("response",response);
-
+  //NOTE: this logic reads:
+  /**
+    if object does not have key
+    then add key && prop
+    else culmulatively add prop
+  */
   response.forEach((object)=>{
     if ( !sumsObj.hasOwnProperty(object.symbol) ){
       sumsObj[object.symbol] = parseFloat(object.pch_units);
@@ -30,6 +34,7 @@ knex('purchases(pch)')
     return purchasedObj;
   })
 
+  //NOTE: highly similar logic in this map
   mapped.forEach((purchasedObj)=>{
     if ( !weightsObj.hasOwnProperty(purchasedObj.symbol) ) {
       weightsObj[purchasedObj.symbol] = parseFloat(purchasedObj.weighted_usd_per_unit)
@@ -60,96 +65,46 @@ knex('purchases(pch)')
     .catch((err)=>{
       console.error('failed to insert in balances table',err);
     })
-
+})
 .then((response)=>{
-  console.log("this is response",response);
-
+  console.log("response",response)
   //unaccounted fees
   let strayBNBFees = [];
-
-  // `select trade_buy,amount-fee as liquid_units,trade_sell,total as costs from trades
-  //  where type = 'BUY';`
 
   return knex('trades')
     .select('date_trade','trade_buy','amount','fee','fee_coin_symbol')
     .where('type','=','BUY')
     .then((response)=>{
       let buys = response;
+      let cryptoFromSelling = {};
+      let cryptoFromBuying = {};
 
       return knex('trades')
         .select('date_trade','trade_sell','amount','fee','fee_coin_symbol')
         .where('type','=','SELL')
         .then((response)=>{
-          return lumpCryptoTogetherSell(response)
-            .then((response)=>{
-              return lumpCryptoTogetherBuy(buys,response)
-            })
+          response.forEach((salesObj)=>{
+            if ( !cryptoFromSelling.hasOwnProperty(salesObj.trade_sell) ) {
+              cryptoFromSelling[salesObj.trade_sell] = parseFloat(salesObj.amount);
+            } else {
+              cryptoFromSelling[salesObj.trade_sell] = parseFloat(salesObj.amount);
+            }
+          })
+          return cryptoFromSelling;
+      })
+      .then((response)=>{
+        console.log("for each cryptocurrency, here is their respective balance",response);
+        buys.forEach((tradeObj)=>{
+          if ( !cryptoFromBuying.hasOwnProperty(tradeObj.trade_buy) ) {
+            cryptoFromBuying[tradeObj.trade_buy] = parseFloat(tradeObj.amount-tradeObj.fee);
+          } else {
+            cryptoFromBuying[tradeObj.trade_buy] += parseFloat(tradeObj.amount-tradeObj.fee);
+          }
         })
-    })
-    .then((response)=>{
-      console.log("for each cryptocurrency, here is their respective balance",response);
-    })
-  //NOTE:  USE THE RESPONSE TO CALCULATE
-  // FOR EACH COIN:
-  // 1. total amount liquid
-  // 2. weighted_usd_per_unit
+        return Object.assign(cryptoFromBuying,cryptoFromSelling);
+      })
+  })
+  .then((response)=>{
+    console.log("here is all the crypto from trading",response);
   })
 })
-
-/**
- * @name - calculateWeighedSums
- * @description - for each crypto purchased, calculates the weighted_usd_per_unit
- * @param - array of purchase objects
- * @param - obj with crypto sums
- * @returns - [['symbol1','sum1','weighted_usd_per_unit1'],
- *             ['symbol2','sum2','weighted_usd_per_unit2']]
- **/
-// function calculateWeighedSums(input,sumsObj){
-//   let weighted = {};
-//   input.forEach((purchaseObj)=>{
-//     purchaseObj['weighted_usd_per_unit'] = ((purchaseObj.pch_units/sumsObj[purchaseObj.symbol])*purchaseObj.pch_usd_per_unit).toFixed(2);
-//   })
-//
-//   input.forEach((purchaseObj)=>{
-//     if ( !weighted[purchaseObj.symbol] ) {
-//       weighted[purchaseObj.symbol] = parseFloat(purchaseObj.weighted_usd_per_unit);
-//     } else {
-//       weighted[purchaseObj.symbol] += parseFloat(purchaseObj.weighted_usd_per_unit);
-//     }
-//   })
-//
-//   //preps data for 'objectification'
-//   return _.zip(Object.keys(sumsObj),Object.values(sumsObj),Object.values(weighted));
-// }
-
-//NOTE: I BET THIS FUNCTION CAN HANDLE BOTH SELL AND BUY CASES FOR TRADES
-async function lumpCryptoTogetherSell(arrayOfObjects){
-  let cryptoFromSelling = {};
-
-  //inside here could go a conditional:
-  //"if trade_sell is not equal to fee_coin_symbol, push relevant info to strayBNBFees"
-  arrayOfObjects.forEach((saleObj)=>{
-    if ( !cryptoFromSelling.hasOwnProperty(saleObj.trade_sell) ){
-      cryptoFromSelling[saleObj.trade_sell] = parseFloat(saleObj.amount);
-    } else {
-      cryptoFromSelling[saleObj.trade_sell] += parseFloat(saleObj.amount);
-    }
-  })
-
-  return cryptoFromSelling;
-}
-
-// NOTE: lumpCryptoTogetherBuy && lumpCryptoTogetherSell MUST UNITE!
-//NOTE: IN FACT, ALL FOUR OF THESE FUNCTIONS PERFORM HIGHLY SIMILAR TASKS
-async function lumpCryptoTogetherBuy(arrayOfObjects,object){
-  let cryptoFromBuying = {};
-  arrayOfObjects.forEach((tradeObj)=>{
-    if ( !cryptoFromBuying.hasOwnProperty(tradeObj.trade_buy) ){
-      cryptoFromBuying[tradeObj.trade_buy] = parseFloat(tradeObj.amount-tradeObj.fee);
-    } else {
-      cryptoFromBuying[tradeObj.trade_buy] += parseFloat(tradeObj.amount-tradeObj.fee);
-    }
-  })
-
-  return Object.assign(cryptoFromBuying,object);
-}
