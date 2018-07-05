398cafa88aa12514a138e151416a4e944ecb0718
jbays <jjbaize@gmail.com>
2018-07-05 11:27:28 -0500

    Removing abstracted functions to really get a look at what needs changing.

--- scripts/updateBalances.js
+++ scripts/updateBalances.js
@@ -12,33 +12,58 @@ knex('purchases(pch)')
   .where('withdrawn','=',false)
   .orderBy('symbol','desc')
 .then((response)=>{
-  return calculateSumsForAllCryptos(response)
-    .then((response)=>{
-      let allCryptoSums = [];
+  let sumsObj = {};
+  let weightsObj = {};
 
-      response.forEach((entry)=>{
-        let obj = {};
+  console.log("response",response);
 
-        obj.symbol = entry[0];
-        obj.liquid_units = entry[1];
-        obj.weighted_usd_per_unit = entry[2];
+  response.forEach((object)=>{
+    if ( !sumsObj.hasOwnProperty(object.symbol) ){
+      sumsObj[object.symbol] = parseFloat(object.pch_units);
+    } else {
+      sumsObj[object.symbol] += parseFloat(object.pch_units);
+    }
+  })
 
-        allCryptoSums.push(obj);
-      })
+  let mapped = response.map((purchasedObj)=>{
+    purchasedObj['weighted_usd_per_unit'] = ((purchasedObj.pch_units/sumsObj[purchasedObj.symbol])*purchasedObj.pch_usd_per_unit).toFixed(2);
+    return purchasedObj;
+  })
 
-      return knex('balances').insert(allCryptoSums)
-        .then((done)=>{
-          console.log('for each crypto purchased,','\n',
-                      'inserted into balance table is:','\n',
-                      'a crypto database entry with fields symbol, sum, and weighted_usd_per_unit');
-        })
-        .catch((err)=>{
-          console.error('failed to insert in balances table',err)
-        })
-    })
+  mapped.forEach((purchasedObj)=>{
+    if ( !weightsObj.hasOwnProperty(purchasedObj.symbol) ) {
+      weightsObj[purchasedObj.symbol] = parseFloat(purchasedObj.weighted_usd_per_unit)
+    } else {
+      weightsObj[purchasedObj.symbol] += parseFloat(purchasedObj.weighted_usd_per_unit)
+    }
+  })
 
+  return _.zip(Object.keys(sumsObj),Object.values(sumsObj),Object.values(weightsObj))
 })
 .then((response)=>{
+  let allCryptoSums = [];
+
+  response.forEach((entry)=>{
+    let obj = {};
+    obj.symbol = entry[0];
+    obj.liquid_units = entry[1];
+    obj.weighted_usd_per_unit = entry[2];
+    allCryptoSums.push(obj);
+  })
+
+  return knex('balances').insert(allCryptoSums)
+    .then((done)=>{
+      console.log('for each crypto purchased,','\n',
+                          'inserted into balance table is:','\n',
+                          'a crypto database entry with fields symbol, sum, and weighted_usd_per_unit');
+    })
+    .catch((err)=>{
+      console.error('failed to insert in balances table',err);
+    })
+
+.then((response)=>{
+  console.log("this is response",response);
+
   //unaccounted fees
   let strayBNBFees = [];
 
@@ -68,26 +93,8 @@ knex('purchases(pch)')
   // FOR EACH COIN:
   // 1. total amount liquid
   // 2. weighted_usd_per_unit
-})
-
-/**
- * @name - calculateSumsForAllCryptos
- * @description - for each crypto purchased, calculates the sum
- * @param - array of purchase objects
- * @returns - {symbolName: sum}
- **/
-async function calculateSumsForAllCryptos(input){
-  let symbols = {};
-  input.forEach((object)=>{
-    if ( !symbols[object.symbol] ) {
-      symbols[object.symbol] = parseFloat(object.pch_units);
-    } else {
-      symbols[object.symbol] += parseFloat(object.pch_units);
-    }
   })
-
-  return calculateWeighedSums(input,symbols);
-}
+})
 
 /**
  * @name - calculateWeighedSums
@@ -97,23 +104,23 @@ async function calculateSumsForAllCryptos(input){
  * @returns - [['symbol1','sum1','weighted_usd_per_unit1'],
  *             ['symbol2','sum2','weighted_usd_per_unit2']]
  **/
-function calculateWeighedSums(input,sumsObj){
-  let weighted = {};
-  input.forEach((purchaseObj)=>{
-    purchaseObj['weighted_usd_per_unit'] = ((purchaseObj.pch_units/sumsObj[purchaseObj.symbol])*purchaseObj.pch_usd_per_unit).toFixed(2);
-  })
-
-  input.forEach((purchaseObj)=>{
-    if ( !weighted[purchaseObj.symbol] ) {
-      weighted[purchaseObj.symbol] = parseFloat(purchaseObj.weighted_usd_per_unit);
-    } else {
-      weighted[purchaseObj.symbol] += parseFloat(purchaseObj.weighted_usd_per_unit);
-    }
-  })
-
-  //preps data for 'objectification'
-  return _.zip(Object.keys(sumsObj),Object.values(sumsObj),Object.values(weighted));
-}
+// function calculateWeighedSums(input,sumsObj){
+//   let weighted = {};
+//   input.forEach((purchaseObj)=>{
+//     purchaseObj['weighted_usd_per_unit'] = ((purchaseObj.pch_units/sumsObj[purchaseObj.symbol])*purchaseObj.pch_usd_per_unit).toFixed(2);
+//   })
+//
+//   input.forEach((purchaseObj)=>{
+//     if ( !weighted[purchaseObj.symbol] ) {
+//       weighted[purchaseObj.symbol] = parseFloat(purchaseObj.weighted_usd_per_unit);
+//     } else {
+//       weighted[purchaseObj.symbol] += parseFloat(purchaseObj.weighted_usd_per_unit);
+//     }
+//   })
+//
+//   //preps data for 'objectification'
+//   return _.zip(Object.keys(sumsObj),Object.values(sumsObj),Object.values(weighted));
+// }
 
 //NOTE: I BET THIS FUNCTION CAN HANDLE BOTH SELL AND BUY CASES FOR TRADES
 async function lumpCryptoTogetherSell(arrayOfObjects){
