68afdaf88c03fde33ca725fb0428f3f302b75e03
jbays <jjbaize@gmail.com>
2018-07-09 19:06:45 -0500

    simplified the logic to calculate sum for each crypto.  then the objects are created (still missing weighted_usd_per_unit) and inserted into the balance table

--- scripts/updateBalanceTrades.js
+++ scripts/updateBalanceTrades.js
@@ -3,11 +3,90 @@ const config = require('../knexfile')[env];
 const knex = require('knex')(config);
 const _ = require('underscore');
 
+let strayBNBFees = [];
+
 knex('trades')
-  .select('*')
+  .distinct('trade_buy')
+  .select()
+  .where('type','=','BUY')
   .then((response)=>{
-    console.log('this is response',response);
+    let allCryptosBought = {};
+    response.forEach((cryptoCurrency)=>{
+      allCryptosBought[cryptoCurrency.trade_buy] = 0;
+    })
+    return allCryptosBought;
   })
+  .then((allCryptosBoughtObj)=>{
+    knex('trades')
+      .select('trade_id','date_trade','trade_buy','amount','fee','total','fee_coin_symbol','trade_sell')
+      .where('type','=','BUY')
+    .then((allTradesBuy)=>{
+      allTradesBuy.forEach((singleTradeBuy)=>{
+        //add amount to trade_buy (symbol)
+        allCryptosBoughtObj[singleTradeBuy.trade_buy] += parseFloat(singleTradeBuy.amount)
+
+        //if trade_buy is equal to fee_coin_symbol
+        if ( singleTradeBuy.trade_buy === singleTradeBuy.fee_coin_symbol ) {
+          //subtract fee from amount
+          allCryptosBoughtObj[singleTradeBuy.trade_buy] -= parseFloat(singleTradeBuy.fee);
+        } else {
+          //NOTE: this BNB fee should be pushed into an array
+          strayBNBFees.push([
+            singleTradeBuy.trade_id,
+            singleTradeBuy.date_trade,
+            parseFloat(singleTradeBuy.fee),
+            singleTradeBuy.fee_coin_symbol,
+            singleTradeBuy.trade_sell
+          ])
+        }
+      })
+      return allCryptosBoughtObj;
+    })
+    .then((allCryptosSums)=>{
+      let insert = [];
+      console.log('allCryptosSums',allCryptosSums);
+
+      Object.keys(allCryptosSums).forEach((singleCryptoSum)=>{
+        console.log('singleCryptoSum',singleCryptoSum)
+        let obj = {};
+        obj.symbol = singleCryptoSum;
+        obj.weighted_usd_per_unit = null;
+        obj.liquid_units = allCryptosSums[singleCryptoSum];
+        obj.from = 'trades_buy';
+        insert.push(obj);
+      })
+
+      knex('balances')
+        .insert(insert)
+        .then((done)=>{
+          console.log('currently calculate the balance from all buy trades')
+          knex.destroy();
+        })
+      console.log(insert);
+    })
+  })
+
+    // let sums = {};
+    // console.log('this is response',response);
+    // response.forEach((singleTrade)=>{
+    //   if ( !sums.hasOwnProperty(singleTrade.trade_buy) ) {
+    //     if ( singleTrade.trade_buy === singleTrade.fee_coin_symbol ) {
+    //       sums[singleTrade.trade_buy] = parseFloat(singleTrade.amount)-parseFloat(singleTrade.fee);
+    //     } else {
+    //       sums[singleTrade.trade_buy] = parseFloat(singleTrade.amount)
+    //       strayBNBFees.push([singleTrade.trade_id,singleTrade.date_trade,singleTrade.fee,singleTrade.fee_coin_symbol])
+    //     }
+    //   } else {
+    //     sums[singleTrade.trade_buy] += parseFloat(singleTrade.amount)
+    //
+    //     if ( !singleTrade.trade_buy === singleTrade.fee_coin_symbol ) {
+    //       strayBNBFees.push([singleTrade.trade_id,singleTrade.date_trade,singleTrade.fee,singleTrade.fee_coin_symbol])
+    //     }
+    //   }
+    // })
+  // })
+
+
 
 // .then((response)=>{
 //   //unaccounted fees
