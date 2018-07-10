dc77e1452e0c0fd6621d3f0166e53dc478e168ca
jbays <jjbaize@gmail.com>
2018-07-09 20:23:08 -0500

    About to make changes to updateBalanceTrades.  Before making objects for insertion into the balance table, you need to know the results from the trade = sell side of the knex query.

--- scripts/updateBalanceTrades.js
+++ scripts/updateBalanceTrades.js
@@ -4,14 +4,15 @@ const knex = require('knex')(config);
 const _ = require('underscore');
 
 let strayBNBFees = [];
+let insertIntoBalancesTable = [];
 
 knex('trades')
   .distinct('trade_buy')
   .select()
   .where('type','=','BUY')
-  .then((response)=>{
+  .then((allUniqueCryptos)=>{
     let allCryptosBought = {};
-    response.forEach((cryptoCurrency)=>{
+    allUniqueCryptos.forEach((cryptoCurrency)=>{
       allCryptosBought[cryptoCurrency.trade_buy] = 0;
     })
     return allCryptosBought;
@@ -20,73 +21,76 @@ knex('trades')
     knex('trades')
       .select('trade_id','date_trade','trade_buy','amount','fee','total','fee_coin_symbol','trade_sell')
       .where('type','=','BUY')
-    .then((allTradesBuy)=>{
-      allTradesBuy.forEach((singleTradeBuy)=>{
-        //add amount to trade_buy (symbol)
-        allCryptosBoughtObj[singleTradeBuy.trade_buy] += parseFloat(singleTradeBuy.amount)
+      .then((allTradesBuy)=>{
+        allTradesBuy.forEach((singleTradeBuy)=>{
+          //add amount to trade_buy (symbol)
+          allCryptosBoughtObj[singleTradeBuy.trade_buy] += parseFloat(singleTradeBuy.amount)
 
-        //if trade_buy is equal to fee_coin_symbol
-        if ( singleTradeBuy.trade_buy === singleTradeBuy.fee_coin_symbol ) {
-          //subtract fee from amount
-          allCryptosBoughtObj[singleTradeBuy.trade_buy] -= parseFloat(singleTradeBuy.fee);
-        } else {
-          //NOTE: this BNB fee should be pushed into an array
-          strayBNBFees.push([
-            singleTradeBuy.trade_id,
-            singleTradeBuy.date_trade,
-            parseFloat(singleTradeBuy.fee),
-            singleTradeBuy.fee_coin_symbol,
-            singleTradeBuy.trade_sell
-          ])
-        }
+          //if trade_buy is equal to fee_coin_symbol
+          if ( singleTradeBuy.trade_buy === singleTradeBuy.fee_coin_symbol ) {
+            //subtract fee from amount
+            allCryptosBoughtObj[singleTradeBuy.trade_buy] -= parseFloat(singleTradeBuy.fee);
+          } else {
+            //NOTE: this BNB fee should be pushed into an array
+            strayBNBFees.push([
+              singleTradeBuy.trade_id,
+              singleTradeBuy.date_trade,
+              parseFloat(singleTradeBuy.fee),
+              singleTradeBuy.fee_coin_symbol,
+              singleTradeBuy.trade_sell
+            ])
+          }
+        })
+        return allCryptosBoughtObj;
       })
-      return allCryptosBoughtObj;
-    })
-    .then((allCryptosSums)=>{
-      let insert = [];
-      console.log('allCryptosSums',allCryptosSums);
 
-      Object.keys(allCryptosSums).forEach((singleCryptoSum)=>{
-        console.log('singleCryptoSum',singleCryptoSum)
-        let obj = {};
-        obj.symbol = singleCryptoSum;
-        obj.weighted_usd_per_unit = null;
-        obj.liquid_units = allCryptosSums[singleCryptoSum];
-        obj.from = 'trades_buy';
-        insert.push(obj);
+  //BEFORE MINTING THE OBJECTS FOR INSERTION INTO BALANCES TABLE,
+  //MUST CORRECTLY CALCULATE THE SUMS
+
+      .then((allCryptosSums)=>{
+        Object.keys(allCryptosSums).forEach((singleCryptoSum)=>{
+          let obj = {};
+          obj.symbol = singleCryptoSum;
+          obj.weighted_usd_per_unit = null;
+          obj.liquid_units = allCryptosSums[singleCryptoSum];
+          obj.from = 'trades_buy';
+          insertIntoBalancesTable.push(obj);
+        })
       })
+  })
+  .then((allCryptoSums)=>{
+    console.log('allCryptoSums',allCryptoSums)
+    knex('trades')
+      .select('trade_id','date_trade','trade_buy','amount','fee','total','fee_coin_symbol','trade_sell')
+      .where('type','=','SELL')
+      .then((response)=>{
+        console.log('this is response',response);
+      })
+  })
+  .then(()=>{
+    //NOTE: before inserting into balances table,
+    //I should calculate weighted_usd_per_unit for these trades.
+
+    //for each eth transaction, I need the price of eth at that moment
+
+    //for each btc transaction, I need the price of btc at that moment
+
+    //knex('trades')
+    //  .select('trade_id','date_trade','trade_sell')
+    //  .where('type','=','BUY')
+
+    /**
+    this is working and fine
 
       knex('balances')
-        .insert(insert)
+        .insert(insertIntoBalancesTable)
         .then((done)=>{
-          console.log('currently calculate the balance from all buy trades')
-          knex.destroy();
-        })
-      console.log(insert);
-    })
+        console.log('currently calculate the balance from all buy trades')
+        knex.destroy();
+      })
+
+    **/
   })
-
-    // let sums = {};
-    // console.log('this is response',response);
-    // response.forEach((singleTrade)=>{
-    //   if ( !sums.hasOwnProperty(singleTrade.trade_buy) ) {
-    //     if ( singleTrade.trade_buy === singleTrade.fee_coin_symbol ) {
-    //       sums[singleTrade.trade_buy] = parseFloat(singleTrade.amount)-parseFloat(singleTrade.fee);
-    //     } else {
-    //       sums[singleTrade.trade_buy] = parseFloat(singleTrade.amount)
-    //       strayBNBFees.push([singleTrade.trade_id,singleTrade.date_trade,singleTrade.fee,singleTrade.fee_coin_symbol])
-    //     }
-    //   } else {
-    //     sums[singleTrade.trade_buy] += parseFloat(singleTrade.amount)
-    //
-    //     if ( !singleTrade.trade_buy === singleTrade.fee_coin_symbol ) {
-    //       strayBNBFees.push([singleTrade.trade_id,singleTrade.date_trade,singleTrade.fee,singleTrade.fee_coin_symbol])
-    //     }
-    //   }
-    // })
-  // })
-
-
 
 // .then((response)=>{
 //   //unaccounted fees
