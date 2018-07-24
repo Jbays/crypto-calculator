362273bf2107ccf7c70f82510f7dd5c2871dc2ca
jbays <jjbaize@gmail.com>
2018-07-24 18:15:21 -0500

    Finished fetching all historical data for trades of trade_type = "Sell" AND "Buy".  What stands between me and calculating profitability for a given cryptocurrency investment?"

--- scripts/findTradeConversions.js
+++ scripts/findTradeConversions.js
@@ -74,7 +74,7 @@ knex('trades')
           bnb_price_usd: responseObj[2].data.coin.price
         })
         .catch((err)=>{
-          console.error('you have an error with trade_conversions update w/trade_id:',response[0])
+          console.error('you have an error with trade_conversions update w/trade_id:',responseObj[0])
         })
       )
     })
@@ -84,16 +84,67 @@ knex('trades')
   .then((response)=>{
     console.log('successfully queried cryptocurrencycharts.com,','\n',
                 'prices in usd at date_time for:','\n',
-                'trade_sell_symbol price','\n',
+                'trade_sell_symbol price(usd)','\n',
                 'and bnb_price')
     knex.destroy();
   })
 
-//for trade_type = SELL,
-//I also need trade_buy_symbol
-// knex('trades')
-//   .join('trades_conversions','trades.trade_id','=','trades_conversions.trade_id')
-//   .select('trades.trade_id','date_trade','trade_buy_symbol')
-//   .then((response)=>{
-//     console.log('this is response',response);
-//   })
+// for trade_type = SELL,
+// I also need trade_buy_symbol
+knex('trades')
+  .join('trades_conversions', 'trades.trade_id', '=', 'trades_conversions.trade_id')
+  .select('trades.trade_id', 'date_trade', 'trade_buy_symbol')
+  .where('trades.trade_type', '=', 'SELL')
+  .then((response) => {
+    return response.map((singleTradeConversion) => {
+      //maps trade_sell_symbol to its cryptocurrencychart's equivalent id
+      singleTradeConversion.trade_buy_symbol = mapCryptoToId[singleTradeConversion.trade_buy_symbol.toLowerCase()];
+      //format ISO with Time to YYYY-MM-DD
+      singleTradeConversion.date_trade = singleTradeConversion.date_trade.toISOString().substring(0, 10);
+      return singleTradeConversion
+    })
+  })
+  .then((response) => {
+    let axiosPromiseArr = [];
+    
+    response.forEach((singleTradeSell, index) => {
+      //helps keep track of the sequence of trade_id -- for axios promise.all (may NOT be necessary)
+      axiosGetTradeIdSequence.push(singleTradeSell.trade_id)
+      
+      //many of these requests are duplicates.  In the future, I should optimize this request builder.
+      //request-builder should remove duplicate api calls.
+      axiosPromiseArr.push(axios.all([
+        singleTradeSell.trade_id,
+        axios.get(`https://www.cryptocurrencychart.com/api/coin/view/${response[index].trade_buy_symbol}/${response[index].date_trade}/USD`, axiosConfig),
+        axios.get(`https://www.cryptocurrencychart.com/api/coin/view/2471/${response[index].date_trade}/USD`, axiosConfig),
+      ]))
+    })
+    
+    return axios.all(axiosPromiseArr)
+  })
+  .then((response) => {
+    let knexPromiseArr = [];
+
+    response.forEach((responseObj) => {
+      knexPromiseArr.push(
+        knex('trades_conversions')
+          .where('trade_id', '=', responseObj[0])
+          .update({
+            usd_per_unit: responseObj[1].data.coin.price,
+            bnb_price_usd: responseObj[2].data.coin.price
+          })
+          .catch((err) => {
+            console.error('you have an error with trade_conversions update w/trade_id:', responseObj[0])
+          })
+      )
+    })
+
+    return Promise.all(knexPromiseArr);
+  })
+  .then((response) => {
+    console.log('successfully queried cryptocurrencycharts.com,', '\n',
+      'prices in usd at date_time for:', '\n',
+      'trade_buy_symbol price(usd)', '\n',
+      'and bnb_price')
+    knex.destroy();
+  })
