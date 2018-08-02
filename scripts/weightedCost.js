a8b5710f3da727581454ac8c3802389c9e6b953d
jbays <jjbaize@gmail.com>
2018-08-02 08:43:02 -0500

    Weighted_usd_per_unit is calculating.  Correctly for ada, bnb, dash, nash, xmr, and xrp.  But incorrect for btc, eos, and eth.

--- scripts/weightedCost.js
+++ scripts/weightedCost.js
@@ -53,70 +53,27 @@ knex('trades')
       .orderBy('trade_buy_symbol')
       .then((tradesJoinTableElements)=>{
         let weightsObj = {};
-        
-        // console.log('allCryptoSumsAndSymbols',allCryptoSumsAndSymbols);
 
-        // tradesJoinTableElements.forEach((singleTrade)=>{
-        //   console.log('this is singleTrade',singleTrade);
-        //   let price = parseFloat(singleTrade.price);
-        //   let amount = parseFloat(singleTrade.amount);
-        //   let usdPerUnit = parseFloat(singleTrade.usd_per_unit);
-        //   let sum = allCryptoSumsAndSymbols[singleTrade.trade_buy_symbol];
+        tradesJoinTableElements.forEach((singleTrade)=>{
+          let price = parseFloat(singleTrade.price);
+          let amount = parseFloat(singleTrade.amount);
+          let usdPerUnit = parseFloat(singleTrade.usd_per_unit);
+          let sum = allCryptoSumsAndSymbols[singleTrade.trade_buy_symbol];
+          let fee = ( singleTrade.trade_buy_symbol !== singleTrade.fee_coin_symbol ) ? parseFloat(singleTrade.fee*singleTrade.bnb_price_usd) : 0;
           
-        //   console.log('this is price',price);
-        //   console.log('this is amount',amount);
-        //   console.log('this is usdPerUnit',usdPerUnit);
-        //   console.log('this is sum',sum);
+          // console.log("this is singleTrade",singleTrade);
+          // console.log("this is fee >>>>>>>",fee,'\n');
 
-        //   if ( !weightsObj.hasOwnProperty(singleTrade.trade_buy_symbol) ) {
-        //     weightsObj[singleTrade.trade_buy_symbol] = (price*amount*usdPerUnit)/sum;
-        //   } else {
-        //     weightsObj[singleTrade.trade_buy_symbol] += (price*amount*usdPerUnit)/sum;
-        //   }
+          if ( !weightsObj.hasOwnProperty(singleTrade.trade_buy_symbol) ) {
+            weightsObj[singleTrade.trade_buy_symbol] = ((price*(amount-fee)*usdPerUnit)/sum);
+          } else {
+            weightsObj[singleTrade.trade_buy_symbol] += ((price*(amount-fee)*usdPerUnit)/sum);
+          }
+        })
 
-        // })
-        // console.log('this is weightsObj',weightsObj);
+        //this object contains all the weighted_usd_per_unit for each cryptocurrency.
+        //NOTE: BTC, EOS, and ETH have screwy numbers.
+        console.log('this is weightsObj',weightsObj);
         
       })
   })
-
-
-//       .then((allCryptoSumsProper)=>{
-//         console.log('allCryptoSumsProper', allCryptoSumsProper)
-
-//         // //each unique crypto now has a corresponding (correct) liquid_units
-//         // console.log('this is allCryptoSumsProper',allCryptoSumsProper);
-
-//         // //fetch trades_conversions
-//         // knex('trades')
-//         //   .join('trades_conversions','trades.trade_id','=','trades_conversions.trade_id')
-//         //   .then((allTradesWithConversions)=>{
-//         //     //calculate weighted_usd_per_unit for each cryptocurrency
-//         //     let allTradesJoined = tradesWithoutConversions.concat(...allTradesWithConversions);
-//         //     let weightedArr = [];
-
-//         //     //1 AUGUST 2018.  O(n^2) time complexity.  :(
-//         //     Object.keys(allCryptoSumsProper).forEach((singleCrypto)=>{
-//         //       let weighted_usd_per_unit = 0;
-//         //       // console.log('singleCrypto',singleCrypto);
-              
-//         //       allTradesJoined.forEach((singleTrade)=>{
-//         //         if ( singleTrade.trade_buy_symbol === singleCrypto ) {
-//         //           // console.log('singleTrade',singleTrade)
-//         //           if ( singleTrade.hasOwnProperty('usd_per_unit') ){
-//         //             console.log('allCryptoSumsProper[singleCrypto]',allCryptoSumsProper[singleCrypto])
-//         //             // weighted_cost_per_unit += (amount / sum) * (usd_per_unit * price)
-//         //             weighted_usd_per_unit += (parseFloat(singleTrade.amount)/allCryptoSumsProper[singleCrypto])*(parseFloat(singleTrade.usd_per_unit)*parseFloat(singleTrade.price));
-//         //           } else {
-//         //             // weighted_cost_per_unit += (amount / sum);
-//         //             weighted_usd_per_unit += (parseFloat(singleTrade.amount)/allCryptoSumsProper[singleCrypto]);
-//         //           }
-//         //         }
-//         //       })
-
-//         //       weightedArr.push(weighted_usd_per_unit);
-//         //     })
-//         //     console.log('weightedArr',weightedArr);
-//         //   })
-//       })
-//   })
