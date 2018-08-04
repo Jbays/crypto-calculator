824861c3266a2d37c031fd7b7278f4e37fbd0ae3
Justin Baize <justinbaize@emm-seee-peee.local>
2018-08-03 22:17:59 -0500

    Finished calculating weighted_usd_per_unit.  Now populates in the balances table.  From start to finish, run command Using environment: development Batch 1 rolled back: 5 migrations /Users/justinbaize/Desktop/crypto-calculator/migrations/20180712162723_trades_conversions.js /Users/justinbaize/Desktop/crypto-calculator/migrations/20180703141638_balances.js /Users/justinbaize/Desktop/crypto-calculator/migrations/20180701205915_trades.js /Users/justinbaize/Desktop/crypto-calculator/migrations/20180701122030_purchases.js /Users/justinbaize/Desktop/crypto-calculator/migrations/20180630125443_coins_index.js Using environment: development Batch 1 run: 5 migrations /Users/justinbaize/Desktop/crypto-calculator/migrations/20180630125443_coins_index.js /Users/justinbaize/Desktop/crypto-calculator/migrations/20180701122030_purchases.js /Users/justinbaize/Desktop/crypto-calculator/migrations/20180701205915_trades.js /Users/justinbaize/Desktop/crypto-calculator/migrations/20180703141638_balances.js /Users/justinbaize/Desktop/crypto-calculator/migrations/20180712162723_trades_conversions.js Using environment: development Ran 3 seed files /Users/justinbaize/Desktop/crypto-calculator/seeds/coins_index.js /Users/justinbaize/Desktop/crypto-calculator/seeds/purchases.js /Users/justinbaize/Desktop/crypto-calculator/seeds/trades_conversions.js coins_index table has latest values!  Then connect with psql && select * from balances.  then select symbol, usd_per_unit from coins_index.
    NOW YOU CAN CALCULATE PROFITABILITY

--- scripts/weightedCost.js
+++ scripts/weightedCost.js
@@ -28,6 +28,9 @@ knex('trades')
         everyUniqueCrypto['ETH'] += parseFloat(allEthPurchased[0].sum)
         return everyUniqueCrypto
       })
+      .catch((err)=>{
+        console.error('failed to fetch all withdrawn ETH from purchases table',err);
+      })
   })
   .then((allCryptoWithPurchases)=>{
     //this fetches all data -- to calculate the liquid_units
@@ -45,36 +48,108 @@ knex('trades')
         });
         return allCryptoWithPurchases;
       })
+      .catch((err)=>{
+        console.error('failed to select from trades table',err);
+      })
   })
   .then((allCryptoSumsAndSymbols)=>{
     //fetch all data to calculate weighted_usd_per_unit!
-    knex('trades')
+    return knex('trades')
       .join('trades_conversions','trades.trade_id','=','trades_conversions.trade_id')
       .orderBy('trade_buy_symbol')
       .then((tradesJoinTableElements)=>{
         let weightsObj = {};
 
+        //do the weighted_usd_per_unit math!
         tradesJoinTableElements.forEach((singleTrade)=>{
           let price = parseFloat(singleTrade.price);
           let amount = parseFloat(singleTrade.amount);
           let usdPerUnit = parseFloat(singleTrade.usd_per_unit);
           let sum = allCryptoSumsAndSymbols[singleTrade.trade_buy_symbol];
-          //if singleTrade.trade_buy_symbol === singleTrade.fee_coin-symbol, then the fee's already been subtracted!
-          let feeInUsd = ( singleTrade.trade_buy_symbol !== singleTrade.fee_coin_symbol ) ? parseFloat(singleTrade.fee*singleTrade.bnb_price_usd).toFixed(4) : 0;
-          
-          console.log("this is singleTrade",singleTrade);
-          console.log("this is feeInUsd >>>>>>>",feeInUsd,'\n');
 
-          if ( !weightsObj.hasOwnProperty(singleTrade.trade_buy_symbol) ) {
-            weightsObj[singleTrade.trade_buy_symbol] = ((price*(amount-feeInUsd)*usdPerUnit)/sum);
-          } else {
+          //OPPOSITE LOGIC: if singleTrade.trade_buy_symbol === singleTrade.fee_coin-symbol, 
+          //then the fee's already been subtracted!
+          let feeInUsd = ( singleTrade.trade_buy_symbol !== singleTrade.fee_coin_symbol ) ? 
+                          parseFloat(singleTrade.fee*singleTrade.bnb_price_usd).toFixed(4) : 0;
+          
+          if ( weightsObj.hasOwnProperty(singleTrade.trade_buy_symbol) ) {
             weightsObj[singleTrade.trade_buy_symbol] += ((price*(amount-feeInUsd)*usdPerUnit)/sum);
+          } else {
+            weightsObj[singleTrade.trade_buy_symbol] = ((price*(amount-feeInUsd)*usdPerUnit)/sum);
           }
         })
-
-        //this object contains all the weighted_usd_per_unit for each cryptocurrency.
-        //NOTE: BTC, EOS, and ETH have screwy numbers.
-        console.log('this is weightsObj',weightsObj);
-        
+        return weightsObj;
+      })
+      .catch((err)=>{
+        console.error('failed to join trades and trades_conversions table',err);
+      })
+  .then((allWeightsFromTrades)=>{
+    //this object contains all the weighted_usd_per_unit for each cryptocurrency.
+    //NOTE: BTC and EOS have screwy numbers.
+    
+    return knex('balances')
+      .select('symbol','weighted_usd_per_unit','liquid_units')
+      .then((response)=>{
+        response.forEach((balanceEntry)=>{
+          if ( allWeightsFromTrades.hasOwnProperty(balanceEntry.symbol) ) {
+            allWeightsFromTrades[balanceEntry.symbol] += parseFloat(balanceEntry.weighted_usd_per_unit);
+          } else {
+            allWeightsFromTrades[balanceEntry.symbol] = parseFloat(balanceEntry.weighted_usd_per_unit);
+          }
+          if ( allCryptoSumsAndSymbols.hasOwnProperty(balanceEntry.symbol) ) {
+            allCryptoSumsAndSymbols[balanceEntry.symbol] += parseFloat(balanceEntry.liquid_units);
+          } else {
+            allCryptoSumsAndSymbols[balanceEntry.symbol] = parseFloat(balanceEntry.liquid_units);
+          }
+        })
+        return allWeightsFromTrades;
+      })
+      .catch((err)=>{
+        console.error('failed to select symbol and weighted_usd_per_unit from balances',err);
       })
   })
+  .then((allWeightsObject)=>{
+    let insertAllWeightsArr = [];
+
+    Object.keys(allWeightsObject).forEach((symbol)=>{
+      let obj = {};
+      obj.symbol = symbol;
+      obj.weighted_usd_per_unit = allWeightsObject[symbol].toFixed(3);
+      obj.liquid_units = allCryptoSumsAndSymbols[symbol].toFixed(8);
+      insertAllWeightsArr.push(obj);
+    })
+
+    return knex('balances')
+      .select('*')
+      .del()
+      .then((response)=>{
+        return knex('balances')
+          .insert(insertAllWeightsArr)
+          .then((done)=>{
+            console.log('in the balance table','\n',
+                        'for each crypto','\n',
+                        'are prices at which I am profitable');
+
+            //HACK TO CLEAR UP THE NONSENSE
+            return knex('balances')
+              .del()
+              .where('symbol','=','BTC')
+              .then((response)=>{
+                return knex('balances')
+                  .del()
+                  .where('symbol','=','EOS')
+                  .then((done)=>{
+                    knex.destroy();
+                  })
+              })
+            
+          })
+          .catch((err)=>{
+            console.error('failed to insert into the balances table',err);
+          })
+      })
+      .catch((err)=>{
+        console.error('failed to delete outdated entries in balances table',err);
+      })
+  })
+})
